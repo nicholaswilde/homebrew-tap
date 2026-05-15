9ab3213ca8453d21306b904e0aebe9eeb0845aeb
nιcнolaѕ wιlde <ncwilde43@gmail.com>
2026-05-14 18:57:51 -0700

    feat: update changedetection-mcp-rs to 0.1.5

--- Formula/changedetection-mcp-rs.rb
+++ Formula/changedetection-mcp-rs.rb
@@ -8,23 +8,23 @@ class ChangedetectionMcpRs < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/nicholaswilde/changedetection-mcp-rs/releases/download/v0.1.3/changedetection-mcp-rs-0.1.3-aarch64-apple-darwin.tar.gz"
-      sha256 "ad7b8a6cfdbe4f23d6fce1de8e30f0f4bd8a321e396c7eb9319b2a15e209ebdf"
+      url "https://github.com/nicholaswilde/changedetection-mcp-rs/releases/download/v0.1.5/changedetection-mcp-rs-0.1.5-aarch64-apple-darwin.tar.gz"
+      sha256 "ccd0452214024a7ff0ceb9c5766785f24298077592a25b6dfc0f8c684cf185ad"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/nicholaswilde/changedetection-mcp-rs/releases/download/v0.1.3/changedetection-mcp-rs-0.1.3-x86_64-apple-darwin.tar.gz"
-      sha256 "39a823963c34985cec81a4d7a3b34b8967bca375d7ea695558e735ef7075edaa"
+      url "https://github.com/nicholaswilde/changedetection-mcp-rs/releases/download/v0.1.5/changedetection-mcp-rs-0.1.5-x86_64-apple-darwin.tar.gz"
+      sha256 "cc7bf754c7c2c7d5d216b88b8037c2fe3de79badc8125e66f0c11b2a3b6ccc63"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/nicholaswilde/changedetection-mcp-rs/releases/download/v0.1.3/changedetection-mcp-rs-0.1.3-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "6c1c12724712dfd11349ac1beb6fa01cf2ef51adbf6bb38f90e2f930cf58f2da"
+      url "https://github.com/nicholaswilde/changedetection-mcp-rs/releases/download/v0.1.5/changedetection-mcp-rs-0.1.5-aarch64-unknown-linux-musl.tar.gz"
+      sha256 "2f7ca3215694e8794239435c5946f2ef1cd433a97607ff98a69938ab78500fa1"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/nicholaswilde/changedetection-mcp-rs/releases/download/v0.1.3/changedetection-mcp-rs-0.1.3-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "05d40c73061567496eabd5ad9f73c3fb1f21d8c68ccd7a99e1a92a9ce48745da"
+      url "https://github.com/nicholaswilde/changedetection-mcp-rs/releases/download/v0.1.5/changedetection-mcp-rs-0.1.5-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "5e374d60738c0be8b95e2e01ef4cd101e182ade286a91abdeb240da08cf3b174"
     end
   end
 
