141c12bcbb6fc8b02db540aacd1fad32e7e98f4a
nιcнolaѕ wιlde <ncwilde43@gmail.com>
2026-05-29 06:55:45 -0700

    feat(formula): bump sysmqttd to v0.1.17

--- Formula/sysmqttd.rb
+++ Formula/sysmqttd.rb
@@ -4,21 +4,21 @@
 class Sysmqttd < Formula
   desc "System monitoring & GPIO actuation daemon in Rust with MQTT autodiscovery"
   homepage "https://github.com/nicholaswilde/sysmqttd"
-  version "0.1.12"
+  version "0.1.17"
   license "Apache-2.0"
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/nicholaswilde/sysmqttd/releases/download/v0.1.12/sysmqttd-0.1.12-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "08ebf3698cb270da3839d42d83d8239cdcdcb806d903c223664ff30730027f6b"
+      url "https://github.com/nicholaswilde/sysmqttd/releases/download/v0.1.17/sysmqttd-0.1.17-aarch64-unknown-linux-musl.tar.gz"
+      sha256 "6ef69d3014e46b329512e9b7855f92f4ef6ca57e2be6f9cb984afb4fc8c3fe32"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/nicholaswilde/sysmqttd/releases/download/v0.1.12/sysmqttd-0.1.12-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "f4261f2dad739beef09394a8287853fbf27f1225118f46d8e4c452ba742d7ff6"
+      url "https://github.com/nicholaswilde/sysmqttd/releases/download/v0.1.17/sysmqttd-0.1.17-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "92a7c8990e014d2832787bc8c466c41ab97a14858997adc4fb37b643e64bfc0d"
     end
     if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-      url "https://github.com/nicholaswilde/sysmqttd/releases/download/v0.1.12/sysmqttd-0.1.12-arm-unknown-linux-gnueabihf.tar.gz"
-      sha256 "f58d7928e43c0bbc615ded04a96b1cb3f15e6c7089bfbd276ec45536a0b1960f"
+      url "https://github.com/nicholaswilde/sysmqttd/releases/download/v0.1.17/sysmqttd-0.1.17-arm-unknown-linux-gnueabihf.tar.gz"
+      sha256 "66d4b48fcbe8e2367d652d696b57aebb33897e30d9b9cda8cc28547f60347741"
     end
   end
 
